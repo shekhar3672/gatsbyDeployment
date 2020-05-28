f84d62c00a9530641cc95c3f34e99d374853ba9b
shekhar3672 <jadhavshekhar444@gmail.com>
2020-05-28 12:15:59 +0530

    init change

---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+export default function Contact() {
+  return (
+  		<Layout>
+  		    <h1>Contact Me !</h1>
+      		<p>Send us a message!</p>
+      		<p>Contact No : 9860826511</p>
+      		<p>Email : jadhavshekhar444@gmail.com</p>
+  		</Layout>
+  	)
+}
